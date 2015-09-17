e09a3e2b05f6ee602c7b515528d43eb2a8ebf893
Ershad Kunnakkadan <ershad92@gmail.com>
2015-09-17 10:48:53 +0530

    Adding home page content

--- config/routes.rb
+++ config/routes.rb
@@ -43,12 +43,6 @@ TalentSpace::Application.routes.draw do
     get '/pages' => 'pages#index', as: :pages
   end
 
-  unauthenticated do
-    as :user do
-      root :to => 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
-
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       devise_scope :user do

531579d0d7b966fbd2edce1b44b8eb08fe873693
Ershad Kunnakkadan <ershad92@gmail.com>
2015-09-18 09:03:22 +0530

    Fixing JavaScript issue

--- app/assets/javascripts/application_common.js
+++ app/assets/javascripts/application_common.js
@@ -4,12 +4,3 @@
 //= require ace-rails-ap
 //
 //= require_tree ./common
-
-
-var editor = ace.edit("sample_program");
-
-var textarea = $('textarea[name="submission[sample_program]"]').hide();
-editor.getSession().setValue(textarea.val());
-editor.getSession().on('change', function(){
-  textarea.val(editor.getSession().getValue());
-});

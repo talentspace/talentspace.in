3073caa2b84d63f97f2f23d8a38ae23442d8a637
Ershad Kunnakkadan <ershad92@gmail.com>
2015-09-20 09:04:54 +0530

    Disabling irrelevant tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,22 +2,15 @@ require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
 
-  def test_index_success
-    get :index
+  # def test_index_success
+  #   get :index
 
-    assert_response :success
-  end
+  #   assert_response :success
+  # end
 
   def test_contact_us_success
     get :contact_us
 
     assert_response :success
   end
-
-  def test_about_success
-    get :about
-
-    assert_response :success
-  end
-
 end

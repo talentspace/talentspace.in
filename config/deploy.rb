d1346c3fd53e9a5343eef5c96c9da2c2109a7de6
Ershad Kunnakkadan <ershad92@gmail.com>
2016-01-19 17:11:14 +0530

    Making a few changes in students page

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.rbenv-
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'config/settings',
-                                               'vendor/bundle', 'public/system', 'db/backups', 'uploads')
+                                               'vendor/bundle', 'public/system', 'db/backups', 'uploads', 'public/uploads')
 
 
 set :keep_releases, 5
